03616624339ef2e07e3f15c9a3ea8a28113efe4e
Fuzzy <zhuravel.1992@gmail.com>
2025-07-19 19:09:55 +0200

    Меню теперь перестало выбрасывать ошибку (нужно было передавать сканнер), попробовал написать алгоритм шифрования (он же алгоритм дешифрования при переменной direction -1), теперь нужно подтянуть как-то к нему файл и всё это связать в одну кучу - ибо от нефункционального меню толку немного...

--- src/main/java/ru/javarush/zhuravel/cryptoanalizer/Cipher.java
+++ src/main/java/ru/javarush/zhuravel/cryptoanalizer/Cipher.java
@@ -14,33 +14,26 @@ public class Cipher {
     private static final int RU_ALPHABET_LENGTH = RU_ALPHABET.length();
 
     public static List<String> encrypt(int direction, List<String> inputTextList, int key, Lang lang) {
-
-        List<String> outputTextList = new ArrayList<String>();
+        List<String> outputTextList = new ArrayList<>();
+        String alphabet = (lang == Lang.EN) ? EN_ALPHABET : RU_ALPHABET;
+        int alphabetLength = alphabet.length();
 
         for (String inputText : inputTextList) {
-            inputText.toLowerCase();
             StringBuilder encryptedLine = new StringBuilder();
+            for (char c : inputText.toCharArray()) {
+                char lowerC = Character.toLowerCase(c);
+                int index = alphabet.indexOf(lowerC);
 
-            char[] charsInput = inputText.toCharArray();
-            for (int i = 0; i < charsInput.length; i++) {
-
-                if (lang == Lang.EN) {
-                    for (int j = 0; j < EN_ALPHABET_LENGTH; j++) {
-                        if (charsInput[i] == EN_ALPHABET_CHARS[j]) {
-                            encryptedLine.append(EN_ALPHABET_CHARS[(j + key * direction) % EN_ALPHABET_LENGTH]);
-                        }
-                    }
-                }
-                else if (lang == Lang.RU) {
-                    for (int j = 0; j < RU_ALPHABET_LENGTH; j++) {
-                        if (charsInput[i] == RU_ALPHABET_CHARS[j]) {
-                            encryptedLine.append(RU_ALPHABET_CHARS[(j + key * direction) % RU_ALPHABET_LENGTH]);
-                        }
-                    }
+                if (index != -1) {
+                    int newIndex = (index + key * direction) % alphabetLength;
+                    if (newIndex < 0) newIndex += alphabetLength;
+                    char newChar = alphabet.charAt(newIndex);
+                    encryptedLine.append(Character.isUpperCase(c) ? Character.toUpperCase(newChar) : newChar);
+                } else {
+                    encryptedLine.append(c);
                 }
             }
-
-        outputTextList.add(encryptedLine.toString());
+            outputTextList.add(encryptedLine.toString());
         }
         return outputTextList;
     }
